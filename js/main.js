3eebeee79c340cd3daff28c05e0173e2a670d92e
Ilya-Saldeckiy <melllllowtoy@yandex.ru>
2021-11-18 10:34:32 +0300

    fix

--- js/main.js
+++ js/main.js
@@ -1,89 +1,10 @@
-
-function slowScroll(id) {
-	let top = $(id).offset().top;
-	$('html, body').animate({
-		scrollTop: top
-	}, {
-		duration: 350,
-		easing: "linear"
-	});
+function copytext(el) {
+	var $tmp = $("<textarea>");
+	$("body").append($tmp);
+	$tmp.val($(el).text()).select();
+	document.execCommand("copy");
+	$tmp.remove();
 }
-
-$(document).ready(function(){
-	$(":input").inputmask();
-
-	//email mask
-	$("#floatingInput3").inputmask({
-		mask: "*{1,20}[.*{1,20}][.*{1,20}][.*{1,20}]@*{1,20}[.*{2,6}][.*{1,2}]",
-		showMaskOnHover: false,
-		showMaskOnFocus: true,
-		greedy: false,
-		onBeforePaste: function (pastedValue, opts) {
-			pastedValue = pastedValue.toLowerCase();
-			return pastedValue.replace("mailto:", "");
-		},
-		definitions: {
-			'*': {
-				validator: "[0-9A-Za-z!#$%&'*+/=?^_`{|}~\-]",
-				casing: "lower"
-			}
-		}
-	});
-
-	$("#floatingInput2").inputmask({
-		mask: '8-(999)-999-9999',
-		placeholder: '8-(___)-___-____',
-		showMaskOnHover: false,
-		showMaskOnFocus: true,
-		onBeforePaste: function (pastedValue, opts) {
-			var processedValue = pastedValue;
-
-//do something with it
-
-			return processedValue;
-		}
-	});
-	$("#floatingInput11").inputmask({
-		mask: '8-(999)-999-9999',
-		placeholder: '8-(___)-___-____',
-		showMaskOnHover: false,
-		showMaskOnFocus: true,
-		onBeforePaste: function (pastedValue, opts) {
-			var processedValue = pastedValue;
-
-//do something with it
-
-			return processedValue;
-		}
-	});
-	$("#floatingInput6").inputmask({
-		mask: '99.99.9999 - 99.99.9999',
-		placeholder: '__.__.____ - __.__.____',
-		showMaskOnHover: false,
-		showMaskOnFocus: true,
-		onBeforePaste: function (pastedValue, opts) {
-			var processedValue = pastedValue;
-
-//do something with it
-
-			return processedValue;
-		}
-	});
-// 	$("#floatingInput8").inputmask({
-// 		mask: '*{1,20}',
-// 		placeholder: '',
-// 		showMaskOnHover: false,
-// 		showMaskOnFocus: true,
-// 		onBeforePaste: function (pastedValue, opts) {
-// 			var processedValue = pastedValue;
-//
-// //do something with it
-//
-// 			return processedValue;
-// 		}
-// 	});
-});
-
 
 
 
--- js/video.js
+++
@@ -1,4 +0,0 @@
-document.querySelector('.video-position').addEventListener('click', function (){
-        document.querySelector('.video__preview').classList.remove("remove__preview");
-        document.querySelector('.video').classList.add('active__video');
-})
